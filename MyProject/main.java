d6cef63fbfcf19ad3ee6801826f5d4e95a0d9c56
Poojitha <poojithapaladugula1@gmail.com>
2026-02-27 10:57:18 +0530

    Bug fix update

--- MyProject/main.java
+++ MyProject/main.java
@@ -2,5 +2,6 @@ package MyProject;
 public class main {
     public static void main(String[] args) {
         System.out.println("Feature update added");
+        System.out.println("Bug fixed successfully");git status
     }
 }
